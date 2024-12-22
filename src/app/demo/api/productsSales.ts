8095a61025d7c60997840c527aa46b8b816eeb43
brenohsilva <brennohsilva@hotmail.com>
2024-12-21 23:26:12 -0300

    :sparkles: create stocks dtos

---
+++ src/app/demo/api/productsSales.ts
@@ -0,0 +1,7 @@
+export interface ProductSales {
+    id?: string;
+    amount?: number;
+    paymentMethod: string;
+    coast: number;
+    totalValue: number;
+}
--- src/app/pages/list/stocks.module.ts
+++ src/app/pages/list/stocks.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {StocksComponent } from './stocks.component';
-import {StockRoutingModule } from './stocks-routing.module';
+import { StocksComponent } from './stocks.component';
+import { StockRoutingModule } from './stocks-routing.module';
 import { DataViewModule } from 'primeng/dataview';
 import { PickListModule } from 'primeng/picklist';
 import { OrderListModule } from 'primeng/orderlist';
@@ -10,20 +10,26 @@ import { InputTextModule } from 'primeng/inputtext';
 import { DropdownModule } from 'primeng/dropdown';
 import { RatingModule } from 'primeng/rating';
 import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { InputNumberModule } from 'primeng/inputnumber';
 
 @NgModule({
-	imports: [
-		CommonModule,
-		FormsModule,
-		StockRoutingModule,
-		DataViewModule,
-		PickListModule,
-		OrderListModule,
-		InputTextModule,
-		DropdownModule,
-		RatingModule,
-		ButtonModule
-	],
-	declarations: [StocksComponent]
+    imports: [
+        CommonModule,
+        FormsModule,
+        StockRoutingModule,
+        DataViewModule,
+        PickListModule,
+        OrderListModule,
+        InputTextModule,
+        DropdownModule,
+        RatingModule,
+        RadioButtonModule,
+		InputNumberModule,
+        ButtonModule,
+        DialogModule,
+    ],
+    declarations: [StocksComponent],
 })
-export class StocksModule { }
+export class StocksModule {}
