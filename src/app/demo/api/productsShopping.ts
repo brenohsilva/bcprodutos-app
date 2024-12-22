548b77e2d24f8cd7dc2f7c670884b50aa4891fa6
brenohsilva <brennohsilva@hotmail.com>
2024-12-21 23:26:12 -0300

    :sparkles: create stocks dtos

---
+++ src/app/demo/api/productsShopping.ts
@@ -0,0 +1,9 @@
+export interface ProductShopping {
+    id?: string;
+    amount?: number;
+    price?: number;
+    paymentMethod?: string;
+    installment?: number;
+    tax?: number;
+    totalValue: number;
+}
--- src/app/pages/list/stocks.component.ts
+++ src/app/pages/list/stocks.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { SelectItem } from 'primeng/api';
 import { DataView } from 'primeng/dataview';
 import { Product } from 'src/app/demo/api/product';
+import { ProductSales } from 'src/app/demo/api/productsSales';
+import { ProductShopping } from 'src/app/demo/api/productsShopping';
 import { ProductService } from 'src/app/demo/service/product.service';
 
 @Component({
-    templateUrl: './stocks.component.html'
+    templateUrl: './stocks.component.html',
 })
 export class StocksComponent implements OnInit {
-
     products: Product[] = [];
 
     sortOptions: SelectItem[] = [];
@@ -17,40 +18,58 @@ export class StocksComponent implements OnInit {
 
     sortField: string = '';
 
-    sourceCities: any[] = [];
+    product: Product = {};
 
-    targetCities: any[] = [];
+    productSalesDialog: boolean = false;
 
-    orderCities: any[] = [];
+    productShoppingDialog: boolean = false;
 
-    constructor(private productService: ProductService) { }
+    paymentMethods: any[] = [];
+
+    installments: any[] = [];
+
+    paymentMethod: any;
+
+    installment: any;
+
+    coust: any;
+
+    productSales: ProductSales;
+
+    productShopping: ProductShopping = {
+        price: 0,
+        tax: 0,
+        amount: 0,
+        totalValue: 0,
+    };
+
+    constructor(private productService: ProductService) {}
 
     ngOnInit() {
-        this.productService.getProducts().then(data => this.products = data);
-
-        this.sourceCities = [
-            { name: 'San Francisco', code: 'SF' },
-            { name: 'London', code: 'LDN' },
-            { name: 'Paris', code: 'PRS' },
-            { name: 'Istanbul', code: 'IST' },
-            { name: 'Berlin', code: 'BRL' },
-            { name: 'Barcelona', code: 'BRC' },
-            { name: 'Rome', code: 'RM' }];
-
-        this.targetCities = [];
-
-        this.orderCities = [
-            { name: 'San Francisco', code: 'SF' },
-            { name: 'London', code: 'LDN' },
-            { name: 'Paris', code: 'PRS' },
-            { name: 'Istanbul', code: 'IST' },
-            { name: 'Berlin', code: 'BRL' },
-            { name: 'Barcelona', code: 'BRC' },
-            { name: 'Rome', code: 'RM' }];
+        this.productService
+            .getProducts()
+            .then((data) => (this.products = data));
 
         this.sortOptions = [
             { label: 'Maior Preço', value: '!price' },
-            { label: 'Menor Preço', value: 'price' }
+            { label: 'Menor Preço', value: 'price' },
+        ];
+
+        this.paymentMethods = [
+            { label: 'Dinheiro', value: 'Dinheiro' },
+            { label: 'Cartão de Credito', value: 'Cartão de Credito' },
+            { label: 'Cartão de Debito', value: 'Cartão de Debito' },
+            { label: 'Pix', value: 'Pix' },
+        ];
+
+        this.installments = [
+            { label: 'A vista', value: 'A vista' },
+            { label: 'Parcelado em 1x', value: 1 },
+            { label: 'Parcelado em 2x', value: 2 },
+            { label: 'Parcelado em 3x', value: 3 },
+            { label: 'Parcelado em 4x', value: 4 },
+            { label: 'Parcelado em 5x', value: 5 },
+            { label: 'Parcelado em 6x', value: 6 },
         ];
     }
 
@@ -69,5 +88,37 @@ export class StocksComponent implements OnInit {
     onFilter(dv: DataView, event: Event) {
         dv.filter((event.target as HTMLInputElement).value);
     }
-    
+
+    openNewSales(product: any) {
+        this.product = product;
+        this.productSalesDialog = true;
+    }
+
+    openNewShopping(product: any) {
+        this.product = product;
+        this.productShoppingDialog = true;
+    }
+
+    saveSale() {
+        (this.productSales = {
+            id: this.product.id,
+            amount: this.product.quantity,
+            paymentMethod: this.paymentMethod,
+            coast: this.coust,
+            totalValue: this.product.price * this.product.quantity,
+        }),
+            alert(this.productSales);
+        console.log(this.productSales);
+    }
+
+    saveShopping() {
+        this.productShopping = {
+            ...this.productShopping,
+            totalValue:
+                (this.productShopping.price + this.productShopping.tax) *
+                this.productShopping.amount,
+        };
+        alert(this.productShopping);
+        console.log(this.productShopping)
+    }
 }
