99a5e10984da8ceca7d6ad725c8e2a2cc5adeaad
brenohsilva <brennohsilva@hotmail.com>
2024-12-22 21:26:42 -0300

    :sparkles: create page of sales

---
+++ src/app/pages/sales/sales.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ChartModule } from 'primeng/chart';
+import { MenuModule } from 'primeng/menu';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { StyleClassModule } from 'primeng/styleclass';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { SalesRoutingModule } from './sales-routing.module';
+import { SalesComponent } from './sales.component';
+
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        ChartModule,
+        MenuModule,
+        TableModule,
+        StyleClassModule,
+        PanelMenuModule,
+        ButtonModule,
+        SalesRoutingModule
+    ],
+    declarations: [SalesComponent]
+})
+export class SalesModule { }
