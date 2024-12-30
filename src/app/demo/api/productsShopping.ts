920345040ae68c1802447d6ee8d14245544bc1f7
brenohsilva <brennohsilva@hotmail.com>
2024-12-30 00:14:58 -0300

    :sparkles: create new shoppnig components

--- src/app/demo/api/productsShopping.ts
+++ src/app/demo/api/productsShopping.ts
@@ -3,7 +3,21 @@ export interface ProductShopping {
     amount?: number;
     price?: number;
     paymentMethod?: string;
-    installment?: number;
     tax?: number;
-    totalValue: number;
+    totalValue?: number;
+}
+
+export interface ShoppingItens {
+    productId: number | string;
+    amount: number;
+    unitPrice: number; //será o subtotal / amount
+    subTotal: number
+}
+export interface Shopping {
+    description?: string;
+    paymentMethod?: string;
+    tax?: number;
+    totalValue?: number; // será o valor dos produtos + o tax
+    shoppingItens?: ShoppingItens[]
+    
 }
---
+++ src/app/pages/shopping/shopping-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ShoppingComponent } from './shopping.component';
+
+
+@NgModule({
+    imports: [RouterModule.forChild([
+        { path: '', component: ShoppingComponent }
+    ])],
+    exports: [RouterModule]
+})
+export class ShoppingRoutingModule { }
