0caceed36d8271b63a2cf66d63c1f1db7fceda73
brenohsilva <brennohsilva@hotmail.com>
2024-12-30 23:02:38 -0300

    :bug: fix bugs in shopping and sales in stocks

--- src/app/pages/list/stocks.component.ts
+++ src/app/pages/list/stocks.component.ts
@@ -3,8 +3,8 @@ import { SelectItem } from 'primeng/api';
 import { DataView } from 'primeng/dataview';
 import { Table } from 'primeng/table';
 import { Product } from 'src/app/demo/api/product';
-import { Sales, UniqueSale } from 'src/app/demo/api/productsSales';
-import { ProductShopping } from 'src/app/demo/api/productsShopping';
+import { Sales } from 'src/app/demo/api/productsSales';
+import { ProductShopping, Shopping } from 'src/app/demo/api/productsShopping';
 import { ProductService } from 'src/app/demo/service/product.service';
 
 @Component({
@@ -17,7 +17,7 @@ export class StocksComponent implements OnInit {
     sortOptions: SelectItem[] = [];
     sortOrder: number = 0;
     sortField: string = '';
-   
+
     //----------------------------
 
     product: Product = {};
@@ -26,56 +26,33 @@ export class StocksComponent implements OnInit {
 
     installments: any[] = [];
 
-    paymentMethod: any;
-
-    installment: any;
-
-    coust: any;
+    salesAmount: number;
 
     salesDialog: boolean = false;
 
     shoppingDialog: boolean = false;
 
-    Sales: UniqueSale;
+    sales: Sales = {
+        description: 'Venda Única',
+        paymentMethod: 'Pix',
+        coast: 0,
+        discount: 0,
+    };
 
-    productShopping: ProductShopping = {
-        price: 0,
+    shopping: Shopping = {
+        description: 'Compra Única',
+        installment: 'A vista',
+        paymentMethod: 'Cartão de Credito',
         tax: 0,
-        amount: 0,
         totalValue: 0,
     };
 
-    selectedProducts: Product[] = [];
+    shoppingProductAmount: number = 1;
 
     constructor(private productService: ProductService) {}
 
     ngOnInit() {
-        this.productService
-            .getProducts()
-            .then((data) => (this.products = data));
-
-        this.sortOptions = [
-            { label: 'Maior Preço', value: '!price' },
-            { label: 'Menor Preço', value: 'price' },
-        ];
-
-        this.paymentMethods = [
-            { label: 'Dinheiro', value: 'Dinheiro' },
-            { label: 'Cartão de Credito', value: 'Cartão de Credito' },
-            { label: 'Cartão de Debito', value: 'Cartão de Debito' },
-            { label: 'Pix', value: 'Pix' },
-        ];
-
-        this.installments = [
-            { label: 'A vista', value: 'A vista' },
-            { label: 'Parcelado em 1x', value: 1 },
-            { label: 'Parcelado em 2x', value: 2 },
-            { label: 'Parcelado em 3x', value: 3 },
-            { label: 'Parcelado em 4x', value: 4 },
-            { label: 'Parcelado em 5x', value: 5 },
-            { label: 'Parcelado em 6x', value: 6 },
-        ];
-
+        this.initialData()
     }
 
     onSortChange(event: any) {
@@ -99,35 +76,101 @@ export class StocksComponent implements OnInit {
         this.salesDialog = true;
     }
 
+    closeNewsales(){
+        this.salesDialog = false;
+    }
+
+    calculateTotalValueOfSales(): number {
+        const subTotal = (this.product.price || 0) * (this.salesAmount || 1);
+        const discount = this.sales.discount || 0;
+        const coast = this.sales.coast || 0;
+        return subTotal - discount - coast;
+    }
+
     openNewShopping(product: any) {
         this.product = product;
         this.shoppingDialog = true;
     }
 
-    saveSale() {
-        (this.Sales = {
-            id: this.product.id,
-            amount: this.product.quantity,
-            paymentMethod: this.paymentMethod,
-            coast: this.coust,
-            totalValue: this.product.price * this.product.quantity,
-        }),
-            alert(this.Sales);
-        console.log(this.Sales);
+    closeNewShopping(){
+        this.shoppingDialog = false;
     }
 
-    saveShopping() {
-        this.productShopping = {
-            ...this.productShopping,
-            totalValue:
-                (this.productShopping.price + this.productShopping.tax) *
-                this.productShopping.amount,
+    saveSale() {
+        const subTotal = (this.product.price || 0) * (this.salesAmount || 1);
+        const discount = this.sales.discount || 0;
+        const coast = this.sales.coast || 0;
+        const totalValue = subTotal - discount - coast;
+    
+        this.sales = {
+            ...this.sales,
+            subTotal,
+            totalValue,
+            salesItens: [
+                {
+                    productId: this.product.id,
+                    unitPrice: this.product.price,
+                    amount: this.salesAmount,
+                    subTotal,
+                },
+            ],
         };
-        alert(this.productShopping);
-        console.log(this.productShopping)
+    
+        console.log(this.sales);
+    }
+    
+
+    saveShopping() {
+
+        this.shopping = {
+            ...this.shopping,
+            totalValue: this.shopping.totalValue + + this.shopping.tax,
+            shoppingItens: [
+                {
+                    productId: this.product.id,
+                    amount: this.shoppingProductAmount,
+                    unitPrice:
+                        this.shopping.totalValue / this.shoppingProductAmount,
+                    subTotal: this.shopping.totalValue,
+                },
+            ],
+        };
+
+        console.log(this.shopping);
     }
 
     onGlobalFilter(table: Table, event: Event) {
-            table.filterGlobal((event.target as HTMLInputElement).value, 'contains');
-        }
+        table.filterGlobal(
+            (event.target as HTMLInputElement).value,
+            'contains'
+        );
+    }
+
+    initialData(){
+        this.productService
+        .getProducts()
+        .then((data) => (this.products = data));
+
+    this.sortOptions = [
+        { label: 'Maior Preço', value: '!price' },
+        { label: 'Menor Preço', value: 'price' },
+    ];
+
+    this.paymentMethods = [
+        { label: 'Dinheiro', value: 'Dinheiro' },
+        { label: 'Cartão de Credito', value: 'Cartão de Credito' },
+        { label: 'Cartão de Debito', value: 'Cartão de Debito' },
+        { label: 'Pix', value: 'Pix' },
+    ];
+
+    this.installments = [
+        { label: 'A vista', value: 'A vista' },
+        { label: 'Parcelado em 1x', value: 1 },
+        { label: 'Parcelado em 2x', value: 2 },
+        { label: 'Parcelado em 3x', value: 3 },
+        { label: 'Parcelado em 4x', value: 4 },
+        { label: 'Parcelado em 5x', value: 5 },
+        { label: 'Parcelado em 6x', value: 6 },
+    ];
+    }
 }
