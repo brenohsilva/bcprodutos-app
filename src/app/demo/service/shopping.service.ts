5aad7ca94bccc7d98ad94a02c41f7933693a80a5
brenohsilva <brennohsilva@hotmail.com>
2025-02-06 17:21:11 -0300

    :recycle: using shopping service and get data

--- src/app/demo/service/shopping.service.ts
+++ src/app/demo/service/shopping.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -9,8 +9,25 @@ export class ShoppingService {
     private readonly base_url = 'http://localhost:3000/';
 
     registerShopping(body: any): Observable<any> {
-        console.log(body)
-        return this.http.post<any>(`${this.base_url}shopping`, body)
-            
+        console.log(body);
+        return this.http.post<any>(`${this.base_url}shopping`, body);
+    }
+
+    getValueShoppingByPeriod(period: string): Observable<any> {
+        const params = new HttpParams().set('range', period);
+        return this.http.get<any>(`${this.base_url}shopping/values`, { params });
+    }
+
+    getQuantityOfProductPurchasedByPeriod(period: string): Observable<any> {
+        const params = new HttpParams().set('range', period);
+        return this.http.get<any>(
+            `${this.base_url}shopping/quantity-of-products-purchased`,
+            { params }
+        );
+    }
+
+    getQuantityOfShoppingByPeriod(period: string): Observable<any> {
+        const params = new HttpParams().set('range', period);
+        return this.http.get<any>(`${this.base_url}shopping/amount`, { params });
     }
 }
