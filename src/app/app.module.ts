7fdcdf1089fbcce9b6bbbafb529d83d57ba2a93a
brenohsilva <brennohsilva@hotmail.com>
2025-02-12 20:39:32 -0300

    :recycle: refactoring the structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppLayoutModule } from './layout/app.layout.module';
 import { NotfoundComponent } from './demo/components/notfound/notfound.component';
 import { ProductService } from './demo/service/product.service';
-import { CountryService } from './demo/service/country.service';
-import { CustomerService } from './demo/service/customer.service';
-import { EventService } from './demo/service/event.service';
-import { IconService } from './demo/service/icon.service';
-import { NodeService } from './demo/service/node.service';
-import { PhotoService } from './demo/service/photo.service';
 import { ShoppingService } from './demo/service/shopping.service';
 import { SalesService } from './demo/service/sales.service';
 import { DashboardService } from './demo/service/dashboard.service';
@@ -24,12 +18,6 @@ import { DashboardService } from './demo/service/dashboard.service';
     imports: [AppRoutingModule, AppLayoutModule],
     providers: [
         { provide: LocationStrategy, useClass: PathLocationStrategy },
-        CountryService,
-        CustomerService,
-        EventService,
-        IconService,
-        NodeService,
-        PhotoService,
         ProductService,
         ShoppingService,
         SalesService,
