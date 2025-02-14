80dd76eff2ee3565cf035dfddb9a6440548b4e53
brenohsilva <brennohsilva@hotmail.com>
2025-02-14 17:18:26 -0300

    :recycle: set backend url

--- src/app/demo/service/dashboard.service.ts
+++ src/app/demo/service/dashboard.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class DashboardService {
     constructor(private http: HttpClient) {}
 
-    private readonly base_url = 'http://localhost:3000/';
+    private readonly base_url = environment.BASE_URL;
 
     getProductsStockQuantity() : Observable<any>{
         return this.http.get<any>(`${this.base_url}products/stocks/quantity`)
--- src/app/demo/service/sales.service.ts
+++ src/app/demo/service/sales.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class SalesService {
     constructor(private http: HttpClient) {}
 
-    private readonly base_url = 'http://localhost:3000/';
+    private readonly base_url = environment.BASE_URL;
 
     registerSale(body: any): Observable<any> {
         return this.http.post<any>(`${this.base_url}sales`, body)   
