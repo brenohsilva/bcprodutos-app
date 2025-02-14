49b0108b4306e05226bbe60f2ecca7a42b651df1
brenohsilva <brennohsilva@hotmail.com>
2025-02-14 17:10:58 -0300

    :recycle: fix eslint problems

--- src/app/layout/app.menu.component.ts
+++ src/app/layout/app.menu.component.ts
@@ -110,33 +110,6 @@ export class AppMenuComponent implements OnInit {
                     },
                 ],
             },
-            {
-                label: 'Pages',
-                icon: 'pi pi-fw pi-briefcase',
-                items: [
-                    {
-                        label: 'Auth',
-                        icon: 'pi pi-fw pi-user',
-                        items: [
-                            {
-                                label: 'Login',
-                                icon: 'pi pi-fw pi-sign-in',
-                                routerLink: ['/auth/login'],
-                            },
-                            {
-                                label: 'Error',
-                                icon: 'pi pi-fw pi-times-circle',
-                                routerLink: ['/auth/error'],
-                            },
-                            {
-                                label: 'Access Denied',
-                                icon: 'pi pi-fw pi-lock',
-                                routerLink: ['/auth/access'],
-                            },
-                        ],
-                    },
-                ],
-            },
         ];
     }
 }
